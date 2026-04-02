f6a92b9433af4d5512b29931838fd3e3bc8c24cf
k-fog <59814110+k-fog@users.noreply.github.com>
2026-04-03 02:43:22 +0900

    update nvim config

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 vim.o.number = true
-vim.o.relativenumber = true
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
@@ -15,170 +14,65 @@ vim.o.ttimeoutlen = 10
 vim.o.swapfile = false
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
-vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
-vim.keymap.set('v', 'j', 'gj', { noremap = true, silent = true })
-vim.keymap.set('v', 'k', 'gk', { noremap = true, silent = true })
-vim.keymap.set('n', 'p', 'p`]', { noremap = true })
-vim.keymap.set('n', 'P', 'P`]', { noremap = true })
-vim.keymap.set('n', '<Esc><Esc>', ':<C-u>set nohlsearch<Return>', { silent = true })
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('v', 'j', 'gj')
+vim.keymap.set('v', 'k', 'gk')
+vim.keymap.set('n', 'p', 'p`]')
+vim.keymap.set('n', 'P', 'P`]')
+vim.keymap.set('n', '<Esc><Esc>', '<Cmd>nohlsearch<CR>')
 
-vim.keymap.set('n', '<leader>w', ':w<Return>')
-vim.keymap.set('n', '<leader>q', ':q<Return>')
-vim.keymap.set('n', '<leader>wq', ':wq<Return>')
-
-vim.cmd('packadd vim-jetpack')
-require('jetpack.packer').add {
-    {'tani/vim-jetpack'}, -- bootstrap
-    {'lewis6991/gitsigns.nvim'},
-    {'sindrets/diffview.nvim'},
-    {'ibhagwan/fzf-lua', requires = 'nvim-tree/nvim-web-devicons', config = function()
-        local fzf = require('fzf-lua')
-        fzf.setup()
-        local git_root_cache = {}
-
-        local function is_git_repo()
-            local cwd = vim.fn.getcwd()
-            if git_root_cache[cwd] ~= nil then
-                return git_root_cache[cwd]
-            end
-
-            local output = vim.fn.systemlist('git rev-parse --is-inside-work-tree')
-            local is_git = output[1] == 'true'
-            git_root_cache[cwd] = is_git
-            return is_git
-        end
-
-        vim.keymap.set('n', '<C-p>', function()
-            if is_git_repo() then
-                fzf.git_files()
-            else
-                fzf.files()
-            end
-        end, { noremap = true, silent = true })
-    end},
-    {'nvim-treesitter/nvim-treesitter', branch = 'main', run = ':TSUpdate', config = function()
-        require('nvim-treesitter').setup()
-        require('nvim-treesitter').install({
-            'bash',
-            'c',
-            'cpp',
-            'lua',
-            'markdown',
-            'ocaml',
-            'python',
-        })
-        vim.api.nvim_create_autocmd("FileType", {
-            group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
-            desc = "Try to enable tree-sitter syntax highlighting",
-            pattern = "*", -- run on *all* filetypes
-            callback = function()
-                pcall(function() vim.treesitter.start() end)
-            end,
-        })
-    end},
-    {'neovim/nvim-lspconfig', config = function()
-        vim.lsp.enable('clangd')
-        vim.lsp.enable('ocamllsp')
-    end},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lsp-signature-help'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-cmdline'},
-    {'hrsh7th/nvim-cmp'},
-    {'sindrets/diffview.nvim'},
-    {'stevearc/conform.nvim', config = function()
-        require('conform').setup({
-            formatters_by_ft = {
-                ocaml = { 'ocamlformat' },
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = 'never',
-            },
-        })
-    end},
-}
-
-
--- Set up nvim-cmp.
-local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local cmp = require'cmp'
-
-cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<C-Space>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        },
-
-        ['<Tab>'] = function(fallback)
-            if not cmp.select_next_item() then
-                if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                    cmp.complete()
-                else
-                    fallback()
-                end
-            end
-        end,
-
-        ['<S-Tab>'] = function(fallback)
-            if not cmp.select_prev_item() then
-                if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                    cmp.complete()
-                else
-                    fallback()
-                end
-            end
-        end,
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-    }, {
-        { name = 'buffer' },
-    })
+-- packages
+vim.pack.add({
+    'https://github.com/lewis6991/gitsigns.nvim',
+    'https://github.com/nvim-mini/mini.icons',
+    'https://github.com/ibhagwan/fzf-lua',
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/neovim/nvim-lspconfig',
+    { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('*') },
+    'https://github.com/stevearc/conform.nvim',
 })
 
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
+-- mini.icons
+require('mini.icons').setup()
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-})
+-- fzf-lua
+local fzf = require('fzf-lua')
+fzf.setup()
+vim.keymap.set('n', '<C-p>', fzf.files, { silent = true })
 
--- Set up lspconfig.
-vim.lsp.config('*', {
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-})
-
--- ocaml indent config
+-- nvim-treesitter
+require('nvim-treesitter').install({'bash', 'c', 'cpp', 'lua', 'markdown', 'ocaml', 'python'})
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'ocaml',
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-    end,
+    callback = function() pcall(vim.treesitter.start) end,
+})
+
+-- LSP
+vim.lsp.enable('clangd')
+vim.lsp.enable('ocamllsp')
+vim.lsp.enable('rust_analyzer')
+
+-- completion
+require('blink.cmp').setup({
+    completion = {
+        menu = {
+            draw = {
+                columns = {
+                    { "label", "label_description", gap = 1 },
+                    { "kind_icon", gap = 1, "kind" }
+                },
+            },
+        },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    },
+    keymap = { preset = 'super-tab' },
+    signature = { enabled = true },
+})
+
+-- formatter
+require('conform').setup({
+    formatters_by_ft = {
+        ocaml = { 'ocamlformat', lsp_format = 'never' },
+    },
+    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
 })
