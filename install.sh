3dd4e5358cc8fd061aecb9632babba9df8500b7a
k-fog <59814110+k-fog@users.noreply.github.com>
2026-04-03 03:36:11 +0900

    update!

--- install.sh
+++ install.sh
@@ -5,15 +5,19 @@ CONFIG_DIR="$HOME/.config"
 
 # neovim
 mkdir -p "$CONFIG_DIR/nvim"
-ln -s "$CUR/init.lua" "$CONFIG_DIR/nvim/init.lua"
+ln -s "$CUR/nvim/init.lua" "$CONFIG_DIR/nvim/init.lua"
 
 # tmux
 mkdir -p "$CONFIG_DIR/tmux"
-ln -s "$CUR/tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
+ln -s "$CUR/tmux/tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
 
 # zsh
-ln -s "$CUR/.zsh_aliases" "$HOME/.zsh_aliases"
-ln -s "$CUR/.zshrc" "$HOME/.zshrc"
+ln -s "$CUR/zsh/.zsh_aliases" "$HOME/.zsh_aliases"
+ln -s "$CUR/zsh/.zshrc" "$HOME/.zshrc"
+
+# sheldon (zsh plugin manager)
+mkdir -p "$CONFIG_DIR/sheldon"
+ln -s "$CUR/sheldon/plugins.toml" "$CONFIG_DIR/sheldon/plugins.toml"
 
 # git
-ln -s "$CUR/.gitconfig" "$HOME/.gitconfig"
+ln -s "$CUR/git/.gitconfig" "$HOME/.gitconfig"
