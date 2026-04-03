47a43ed4bcd3d605ca7a5cb7dd752d3c2434fdd1
k-fog <59814110+k-fog@users.noreply.github.com>
2026-04-03 13:16:18 +0900

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,13 +24,13 @@ vim.keymap.set('n', '<Esc><Esc>', '<Cmd>nohlsearch<CR>')
 
 -- packages
 vim.pack.add({
-    'https://github.com/lewis6991/gitsigns.nvim',
-    'https://github.com/nvim-mini/mini.icons',
-    'https://github.com/ibhagwan/fzf-lua',
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/neovim/nvim-lspconfig',
-    { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('*') },
-    'https://github.com/stevearc/conform.nvim',
+  'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/nvim-mini/mini.icons',
+  'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/neovim/nvim-lspconfig',
+  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('*') },
+  'https://github.com/stevearc/conform.nvim',
 })
 
 -- mini.icons
@@ -39,12 +39,22 @@ require('mini.icons').setup()
 -- fzf-lua
 local fzf = require('fzf-lua')
 fzf.setup()
-vim.keymap.set('n', '<C-p>', fzf.files, { silent = true })
+local git_cache = {}
+vim.keymap.set("n", "<C-p>", function()
+  local cwd = vim.loop.cwd()
+  if git_cache[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null")
+    git_cache[cwd] = (vim.v.shell_error == 0)
+  end
+  if git_cache[cwd] then fzf.git_files() else fzf.files() end
+end)
+vim.keymap.set('n', '<C-\\>', fzf.buffers)
+vim.keymap.set('n', '<leader>f', fzf.live_grep)
 
 -- nvim-treesitter
 require('nvim-treesitter').install({'bash', 'c', 'cpp', 'lua', 'markdown', 'ocaml', 'python'})
 vim.api.nvim_create_autocmd('FileType', {
-    callback = function() pcall(vim.treesitter.start) end,
+  callback = function() pcall(vim.treesitter.start) end,
 })
 
 -- LSP
@@ -54,35 +64,35 @@ vim.lsp.enable('rust_analyzer')
 
 -- completion
 require('blink.cmp').setup({
-    completion = {
-        menu = {
-            draw = {
-                columns = {
-                    { "label", "label_description", gap = 1 },
-                    { "kind_icon", gap = 1, "kind" }
-                },
-            },
+  completion = {
+    menu = {
+      draw = {
+        columns = {
+          { "label", "label_description", gap = 1 },
+          { "kind_icon", gap = 1, "kind" }
         },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      },
     },
-    keymap = { preset = 'super-tab' },
-    signature = { enabled = true },
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+  },
+  keymap = { preset = 'super-tab' },
+  signature = { enabled = true },
 })
 
 -- formatter
 require('conform').setup({
-    formatters_by_ft = {
-        ocaml = { 'ocamlformat', lsp_format = 'never' },
-    },
-    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+  formatters_by_ft = {
+    ocaml = { 'ocamlformat' },
+  },
+  format_on_save = { timeout_ms = 500, lsp_format = 'never' },
 })
 
--- OCaml indent
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'ocaml',
-    callback = function()
-        vim.bo.shiftwidth = 2
-        vim.bo.tabstop = 2
-        vim.bo.softtabstop = 2
-    end,
+-- indent
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua", "ocaml" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 })
