a7080efafbedb949c7ed1705979537c75d5958ff
k-fog <59814110+k-fog@users.noreply.github.com>
2026-04-04 19:28:53 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ CONFIG_DIR="$HOME/.config"
 
 # neovim
 mkdir -p "$CONFIG_DIR/nvim"
-ln -s "$CUR/nvim/init.lua" "$CONFIG_DIR/nvim/init.lua"
+ln -sfn "$CUR/nvim/init.lua" "$CONFIG_DIR/nvim/init.lua"
 
 # tmux
 mkdir -p "$CONFIG_DIR/tmux"
-ln -s "$CUR/tmux/tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
+ln -sfn "$CUR/tmux/tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
 
 # zsh
-ln -s "$CUR/zsh/.zsh_aliases" "$HOME/.zsh_aliases"
-ln -s "$CUR/zsh/.zshrc" "$HOME/.zshrc"
+ln -sfn "$CUR/zsh/.zsh_aliases" "$HOME/.zsh_aliases"
+ln -sfn "$CUR/zsh/.zshrc" "$HOME/.zshrc"
 
 # sheldon (zsh plugin manager)
 mkdir -p "$CONFIG_DIR/sheldon"
-ln -s "$CUR/sheldon/plugins.toml" "$CONFIG_DIR/sheldon/plugins.toml"
+ln -sfn "$CUR/sheldon/plugins.toml" "$CONFIG_DIR/sheldon/plugins.toml"
 
 # git
-ln -s "$CUR/git/.gitconfig" "$HOME/.gitconfig"
+ln -sfn "$CUR/git/.gitconfig" "$HOME/.gitconfig"
